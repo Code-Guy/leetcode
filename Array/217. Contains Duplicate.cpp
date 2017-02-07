f48dc3fe91e5b552d4b150d79d7a857c2674e3f9
RobotBerry <1964875003@qq.com>
2017-02-07 09:47:18 +0800

    Create 217. Contains Duplicate.cpp

---
+++ Array/217. Contains Duplicate.cpp
@@ -0,0 +1,7 @@
+// use set, every element in set is unique
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        return nums.size() > set<int>(nums.begin(), nums.end()).size();
+    }
+};
