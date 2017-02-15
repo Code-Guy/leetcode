c931eab1d9fb423e248fd860442a3d45a56e37e5
RobotBerry <1964875003@qq.com>
2017-02-15 16:49:56 +0800

    Create 55. Jump Game.cpp

---
+++ Array/55. Jump Game.cpp
@@ -0,0 +1,11 @@
+// iterate
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach = 0;
+        for (int i = 0; i < nums.size() && i <= reach; i++) {
+            reach = max(reach, i + nums[i]);
+        }
+        return reach >= nums.size() - 1;
+    }
+};
