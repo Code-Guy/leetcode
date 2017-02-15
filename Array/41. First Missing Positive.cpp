de9a73d8df79f35b8cae89c642a2109c5de16508
RobotBerry <1964875003@qq.com>
2017-02-15 17:24:22 +0800

    Create 41. First Missing Positive.cpp

---
+++ Array/41. First Missing Positive.cpp
@@ -0,0 +1,18 @@
+// place the number on the slot of it's value, the the wrong placed number is the missing number
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+};
