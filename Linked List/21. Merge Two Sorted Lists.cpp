0cc1219607f9b859f7b113fd036186fbea8dd6fd
RobotBerry <1964875003@qq.com>
2017-02-22 10:07:32 +0800

    Create 21. Merge Two Sorted Lists.cpp

---
+++ Linked List/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,57 @@
+// 通常意义上说的sorted array或者sorted linked list都是ascending的，更严谨的应该先判断顺序关系然后再合并
+// 可以利用一个非指针的dummy node
+// Solution 1 travese two linked list sequentially
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(-1);
+        ListNode *tail = &dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val){
+                tail->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
+
+// Solution 2 recursive method
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL) return l2;
+        if (l2 == NULL) return l1;
+        
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
