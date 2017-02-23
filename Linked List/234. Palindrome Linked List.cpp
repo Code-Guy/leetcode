6216b32a8525e10aaf5cb8bf5a797a58e542f58b
RobotBerry <1964875003@qq.com>
2017-02-23 15:35:37 +0800

    Create 234. Palindrome Linked List.cpp

---
+++ Linked List/234. Palindrome Linked List.cpp
@@ -0,0 +1,37 @@
+// similar to Reorder List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+        ListNode *p1 = head, *p2 = head->next;
+        while (p2 && p2->next) {
+            p1 = p1->next;
+            p2 = p2->next->next;
+        }
+        
+        ListNode *cur = p1->next;
+        while (cur->next) {
+            ListNode *tmp = cur->next;
+            cur->next = tmp->next;
+            tmp->next = p1->next;
+            p1->next = tmp;
+        }
+        p2 = p1->next;
+        p1 = head;
+        
+        while (p2) {
+            if (p1->val != p2->val) return false;
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return true;
+    }
+};
